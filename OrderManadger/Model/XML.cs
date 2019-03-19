6ec57577956af3b09ebc6d47e4a3df36a725dae6
Админ <@->
2019-03-19 15:34:14 +0300

    update 4.1

--- OrderManadger/Model/XML.cs
+++ OrderManadger/Model/XML.cs
@@ -14,43 +14,49 @@ namespace OrderManadger.Model
         private static XmlSerializer EntryFormatter = new XmlSerializer(typeof(ObservableCollection<Entry>));
         private static XmlSerializer OptionallyFormatter = new XmlSerializer(typeof(List<string>));
 
-        public static void Save(ObservableCollection<Entry> entries, List<string> seller, List<string> assortment)
+        public static async void Save(ObservableCollection<Entry> entries, List<string> seller, List<string> assortment)
         {
-            using (FileStream fs = new FileStream("base.xml", FileMode.OpenOrCreate))
+            await Task.Run(() =>
             {
-                EntryFormatter.Serialize(fs, entries);
-            }
-            using (FileStream fs = new FileStream("Sellers.xml", FileMode.OpenOrCreate))
-            {
-                OptionallyFormatter.Serialize(fs, seller);
-            }
-            using (FileStream fs = new FileStream("Assortment.xml", FileMode.OpenOrCreate))
-            {
-                OptionallyFormatter.Serialize(fs, assortment);
-            }
+                using (FileStream fs = new FileStream("base.xml", FileMode.OpenOrCreate))
+                {
+                    EntryFormatter.Serialize(fs, entries);
+                }
+                Task.Delay(100);
+                using (FileStream fs = new FileStream("Sellers.xml", FileMode.OpenOrCreate))
+                {
+                    OptionallyFormatter.Serialize(fs, seller);
+                }
+                Task.Delay(100);
+                using (FileStream fs = new FileStream("Assortment.xml", FileMode.OpenOrCreate))
+                {
+                    OptionallyFormatter.Serialize(fs, assortment);
+                }
+                Task.Delay(100);
+            });
         }
         public static ObservableCollection<Entry> LoadEntries()
         {
             if (!File.Exists("base.xml")) return new ObservableCollection<Entry>();
-            using (FileStream fs = new FileStream("base.xml", FileMode.Open))
+            using (FileStream fs1 = new FileStream("base.xml", FileMode.Open))
             {
-                return (ObservableCollection<Entry>)EntryFormatter.Deserialize(fs);
+                return (ObservableCollection<Entry>)EntryFormatter.Deserialize(fs1);
             }
         }
         public static List<string> LoadSellers()
         {
             if (!File.Exists("Sellers.xml")) return new List<string>();
-            using (FileStream fs = new FileStream("Sellers.xml", FileMode.Open))
+            using (FileStream fs2 = new FileStream("Sellers.xml", FileMode.Open))
             {
-                return (List<string>)OptionallyFormatter.Deserialize(fs);
+                return (List<string>)OptionallyFormatter.Deserialize(fs2);
             }
         }
         public static List<string> LoadAssortment()
         {
             if (!File.Exists("Assortment.xml")) return new List<string>();
-            using (FileStream fs = new FileStream("Assortment.xml", FileMode.Open))
+            using (FileStream fs3 = new FileStream("Assortment.xml", FileMode.Open))
             {
-                return (List<string>)OptionallyFormatter.Deserialize(fs);
+                return (List<string>)OptionallyFormatter.Deserialize(fs3);
             }
         }
 
