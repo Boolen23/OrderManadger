6551de78f2fa7975c5f8c962a4bf3950000af62e
dis3122 <dis3122@gmail.com>
2019-03-26 22:41:27 +0300

    update 5.1

--- OrderManadger/Model/DataBase/DataBase.cs
+++ OrderManadger/Model/DataBase/DataBase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OrderManadger.Model.DataBase
@@ -14,7 +15,10 @@ namespace OrderManadger.Model.DataBase
             FirstStart();
         }
         public event EventHandler<DataBaseEventArgs> StatusChanged;
-        string connStr = @"Data Source=(local)\SQLEXPRESS; Initial Catalog=Base; Integrated Security=True";
+       string connStr = @"Data Source=.\SQLEXPRESS; Initial Catalog=T; Integrated Security=True; database=master";
+       // string connStr = @"Server='C:\\MyFolder\\New1.mdf';Integrated security=True;database=master";
+
+
         private SqlConnection conn;
         public async Task<bool> TryConnectAsync(string connect)
         {
@@ -24,7 +28,7 @@ namespace OrderManadger.Model.DataBase
                 await conn.OpenAsync();
                 return true;
             }
-            catch
+            catch (Exception ex)
             {
                 return false;
             }
@@ -39,22 +43,36 @@ namespace OrderManadger.Model.DataBase
             else
             {
                 Status = "База данных не найдена или повреждена." + Environment.NewLine + "Создание базы данных...";
-                await CreateBase();
+               await CreateBase();
                 await CreateTables();
                 Status = "База данных создана!";
             }
         }
         private async Task CreateBase()
         {
-            if (conn != null) conn.Close();
-            conn = new SqlConnection(@"Data Source=(local)\SQLEXPRESS;Integrated Security=True");
-            SqlCommand cmdCreateDataBase = new SqlCommand(string.Format("CREATE DATABASE [{0}]", "Base"), conn);
-            await conn.OpenAsync();
-            await cmdCreateDataBase.ExecuteNonQueryAsync();
+            conn.Close();
+
+            //conn = new SqlConnection(@"Data Source=(local)\SQLEXPRESS;Integrated Security=True");
+            //SqlCommand cmdCreateDataBase = new SqlCommand(string.Format("CREATE DATABASE [{0}]", "nio1234567890"), conn);
+            String str;
+            conn = new SqlConnection("Server=localhost;Integrated security=True;database=master");
+
+            str = "CREATE DATABASE T ON PRIMARY " +
+                "(NAME = T1, " +
+                "FILENAME = 'C:\\MyFolder\\T1.mdf', " +
+                "SIZE = 2MB)";
+
+            SqlCommand myCommand = new SqlCommand(str, conn);
+
+
+
+           await conn.OpenAsync();
+           await  myCommand.ExecuteNonQueryAsync();
+            await Task.Delay(2000);
             conn.Close();
             await Task.Delay(5000);
             conn = new SqlConnection(connStr);
-            await conn.OpenAsync();
+           await conn.OpenAsync();
         }
         private async Task CreateTables()
         {
@@ -89,19 +107,23 @@ namespace OrderManadger.Model.DataBase
 
         public async void Test()
         {
-            SqlCommand cmd = new SqlCommand("INSERT INTO Entry VALUES ('seg','@time', 'srags', 'seGF')",conn);
-            cmd.Parameters.AddWithValue("@time", DateTime.Now);
+            SqlCommand cmd = new SqlCommand(@"show databases;",conn);
             await cmd.ExecuteNonQueryAsync();
-            await Task.Delay(100);
 
-            cmd = new SqlCommand("INSERT INTO Sellers VALUES ('1seg')", conn);
-            await cmd.ExecuteNonQueryAsync();
-            await Task.Delay(100);
 
-            cmd = new SqlCommand("INSERT INTO Assortment VALUES ('2seg')", conn);
-            await cmd.ExecuteNonQueryAsync();
-            await Task.Delay(100);
-            Status = "Запись в базу данных произведена успешно";
+            //SqlCommand cmd = new SqlCommand("INSERT INTO Entry VALUES ('seg','@time', 'srags', 'seGF')",conn);
+            //cmd.Parameters.AddWithValue("@time", DateTime.Now);
+            //await cmd.ExecuteNonQueryAsync();
+            //await Task.Delay(100);
+
+            //cmd = new SqlCommand("INSERT INTO Sellers VALUES ('1seg')", conn);
+            //await cmd.ExecuteNonQueryAsync();
+            //await Task.Delay(100);
+
+            //cmd = new SqlCommand("INSERT INTO Assortment VALUES ('2seg')", conn);
+            //await cmd.ExecuteNonQueryAsync();
+            //await Task.Delay(100);
+            //Status = "Запись в базу данных произведена успешно";
         }
     }
 }
