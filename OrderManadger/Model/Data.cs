65406bf4d4f059bda647613aa1711fb113ab114d
dis3122 <dis3122@gmail.com>
2019-03-28 22:46:21 +0300

    update 5.3

---
+++ OrderManadger/Model/Data.cs
@@ -0,0 +1,25 @@
+﻿using OrderManadger.Model.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManadger.Model
+{
+    public static class Data
+    {
+        private static BinaryFormatter formatter = new BinaryFormatter();
+        public static void Save(ObservableCollection<Entry> _Entrys, List<string> _Sellers, List<string> _Assortment)
+        {
+            if (File.Exists("Entrys.data")) File.Delete("Entrys.data");
+            using (FileStream fs = new FileStream("Entrys.data", FileMode.Create))
+            {
+                formatter.Serialize(fs, new SaveObject(_Entrys, _Sellers, _Assortment));
+            }
+        }
+    }
+}
---
+++ OrderManadger/Model/DataBase/SaveObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManadger.Model.DataBase
+{
+    [Serializable]
+    public class SaveObject
+    {
+        public SaveObject(ObservableCollection<Entry> _Entrys, List<string> _Sellers, List<string> _Assortment)
+        {
+            Entrys = _Entrys;
+            Sellers = _Sellers;
+            Assortment = _Assortment;
+        }
+        public ObservableCollection<Entry> Entrys;
+        public List<string> Sellers;
+        public List<string> Assortment;
+    }
+}
