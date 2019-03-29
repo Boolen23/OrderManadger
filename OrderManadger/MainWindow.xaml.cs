d1435707ce5afb2db9b652e27424fbb6571349bc
Админ <@->
2019-03-29 13:13:06 +0300

    update 6.1

--- OrderManadger/MainWindow.xaml.cs
+++ OrderManadger/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -23,7 +25,77 @@ namespace OrderManadger
         public MainWindow()
         {
             InitializeComponent();
+            ToRight();
         }
-       
+        #region Move
+
+        private bool winDragged = false;
+        private Point lmAbs = new Point();
+
+        void Window_MouseDown(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            winDragged = true;
+            this.lmAbs = e.GetPosition(this);
+            this.lmAbs.Y = Convert.ToInt16(this.Top) + this.lmAbs.Y;
+            this.lmAbs.X = Convert.ToInt16(this.Left) + this.lmAbs.X;
+            Mouse.Capture(this);
+        }
+
+        void Window_MouseUp(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            winDragged = false;
+            Mouse.Capture(null);
+        }
+
+        void Window_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (winDragged)
+            {
+                Point MousePosition = e.GetPosition(this);
+                Point MousePositionAbs = new Point();
+                MousePositionAbs.X = Convert.ToInt16(this.Left) + MousePosition.X;
+                MousePositionAbs.Y = Convert.ToInt16(this.Top) + MousePosition.Y;
+                this.Left = this.Left + (MousePositionAbs.X - this.lmAbs.X);
+                this.Top = this.Top + (MousePositionAbs.Y - this.lmAbs.Y);
+                this.lmAbs = MousePositionAbs;
+            }
+        }
+
+        #endregion
+        #region Bottommost
+
+        [DllImport("user32.dll")]
+        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
+        const UInt32 SWP_NOSIZE = 0x0001;
+        const UInt32 SWP_NOMOVE = 0x0002;
+        const UInt32 SWP_NOACTIVATE = 0x0010;
+
+        private void ToBack()
+        {
+            var handle = new WindowInteropHelper(this).Handle;
+            SetWindowPos(handle, HWND_BOTTOM, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
+        }
+
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+            ToBack();
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            base.OnStateChanged(e);
+            ToBack();
+        }
+
+        #endregion
+        private void ToRight()
+        {
+            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+            this.Left = desktopWorkingArea.Right - this.Width;
+            this.Top = desktopWorkingArea.Bottom - this.Height;
+        }
     }
 }
