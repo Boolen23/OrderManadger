5e20c9d82a43edc5eabe299b9faa9c9da7a6fd7c
Админ <@->
2019-04-11 11:37:05 +0300

    update 13.5

--- OrderManadger/Model/Client/LiveClient.cs
+++ OrderManadger/Model/Client/LiveClient.cs
@@ -22,23 +22,29 @@ namespace OrderManadger.Model.Client
         public event EventHandler<RecivedFrameEventArgs> NewFrameRecived;
         public async void StartConnect()
         {
-            bool resultSucsess = false;
-            await Task.Run(() =>
+            //bool resultSucsess = false;
+            //await Task.Run(() =>
+            //{
+            //    try
+            //    {
+            //        Client.Connect(ipAddr, port);
+            //        resultSucsess = true;
+            //    }
+            //    catch
+            //    {
+            //        Task.Delay(500);
+            //        resultSucsess = false;
+            //    }
+            //});
+            //if (resultSucsess) StartRecive();
+            //else StartConnect();
+            while (true)
             {
-                try
-                {
-                    Client.Connect(ipAddr, port);
-                    resultSucsess = true;
-                }
-                catch
-                {
-                    Task.Delay(500);
-                    resultSucsess = false;
-                }
-            });
-            if (resultSucsess) StartRecive();
-            else StartConnect();
-            //Image = new BitmapImage(new Uri(@"D://bin//1.png"));
+                Image = FOO();
+                X++;
+                await Task.Delay(1);
+            }
+           
         }
         private async void StartRecive()
         {
@@ -66,7 +72,7 @@ namespace OrderManadger.Model.Client
         private IPAddress ipAddr = IPAddress.Parse("192.168.0.50");
         private int port = 11720;
         TcpClient Client;
-        public static BitmapImage ConvertByteArrayToBitmapImage(byte[] bytes)
+        public BitmapImage ConvertByteArrayToBitmapImage(byte[] bytes)
         {
             var stream = new MemoryStream(bytes);
             stream.Seek(0, SeekOrigin.Begin);
@@ -76,5 +82,41 @@ namespace OrderManadger.Model.Client
             image.EndInit();
             return image;
         }
+        public BitmapImage FOO()
+        {
+            DrawingVisual draw = new DrawingVisual();
+            using(DrawingContext dc = draw.RenderOpen())
+            {
+                dc.DrawEllipse(Brushes.Red, new Pen(Brushes.Black, 1), new System.Windows.Point(X, 50), 20, 20);
+            }
+            RenderTargetBitmap rtb = new RenderTargetBitmap(200, 200, 96, 96, PixelFormats.Default);
+            rtb.Render(draw);
+            var bitmapImage = new BitmapImage();
+            var bitmapEncoder = new PngBitmapEncoder();
+            bitmapEncoder.Frames.Add(BitmapFrame.Create(rtb));
+
+            using (var stream = new MemoryStream())
+            {
+                bitmapEncoder.Save(stream);
+                stream.Seek(0, SeekOrigin.Begin);
+
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.StreamSource = stream;
+                bitmapImage.EndInit();
+            }
+            return bitmapImage;
+        }
+        public int x;
+        public int X
+        {
+            get => x;
+            set
+            {
+                x = value;
+                if (x > 200) x = 0;
+            }
+        }
+        
     }
 }
